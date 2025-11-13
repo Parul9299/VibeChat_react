b62cb6c64c82c0ada54cbc8fa873a3ac990b07c0
RaajSahabG <vishalrajsatsangi2003@gmail.com>
2025-11-13 15:06:33 +0530

    reset commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-red-600 text-4xl font-bold">Vishal Raj</h1>
+    </div>
+  )
+}
+
+export default App
